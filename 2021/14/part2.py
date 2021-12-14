a8a5de5b8864f7a0f73155d92a869d669b726515
Hannes Küttner <kuettner.hannes@gmail.com>
2021-12-14 13:55:06 +0100

    2021/14: Fix solution for part 2

--- 2021/14/part2.py
+++ 2021/14/part2.py
@@ -5,7 +5,10 @@ input = open('input.txt', 'r').read().strip().splitlines()
 
 polymer = input[0]
 
-replacements = dict(l.split(" -> ") for l in input[2:])
+replacements = dict()
+for l in input[2:]:
+  k, r = l.split(" -> ")
+  replacements[tuple(k)] = r
 
 pairs = Counter(zip(polymer, polymer[1:]))
 
@@ -22,6 +25,8 @@ for _ in range(40):
 letter_count = defaultdict(int)
 for (_, b), count in pairs.items():
   letter_count[b] += count
+
+letter_count[polymer[0]] += 1
 
 result = max(letter_count.values()) - min(letter_count.values())
 print("Result: {}".format(result))
