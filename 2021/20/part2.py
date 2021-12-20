3874c3d7547f43230fd0206ff0e440cbc45b112d
Hannes Küttner <kuettner.hannes@gmail.com>
2021-12-20 08:29:31 +0100

    2021/20: Use scipy.convolve for speedup

--- 2021/20/part2.py
+++ 2021/20/part2.py
@@ -1,40 +1,18 @@
 import numpy as np
-
-def to_int(a):
-  out = 0
-  for i, d in enumerate(reversed(a)):
-    if d:
-      out |= (1 << i)
-  return out
-
-
-input = open('input.txt', 'r').read().strip().splitlines()
-
-lookup = np.array([c == "#" for c in input[0]], dtype=bool)
+from scipy.ndimage import convolve
 
 STEPS = 50
+input = open('input.txt', 'r').read().strip().splitlines()
 
-size = len(input[2])
+lookup = np.array([int(c == "#") for c in input[0]])
+
 margin = STEPS + 1
-image = np.zeros((size + margin * 2, size + margin * 2))
-image[margin:-margin, margin:-margin] = np.array([[c == "#" for c in l] for l in input[2:]], dtype=bool)
+image = np.pad([[int(c == "#") for c in l] for l in input[2:]], (margin, margin))
 
+bin2dec = 2**np.arange(9).reshape(3,3)
 
-inf_values = False
 for _ in range(STEPS):
-  inf_values = lookup[to_int([inf_values] * 9)]
-  new_image = np.zeros_like(image)
-  
-  for i in range(1, image.shape[0] - 1):
-    for j in range(1, image.shape[1] - 1):
-      index = to_int(image[i-1:i+2, j-1:j+2].flatten())
-      new_image[i, j] = lookup[index]
-      
-  new_image[0, :] = inf_values
-  new_image[-1, :] = inf_values
-  new_image[:, 0] = inf_values
-  new_image[:, -1] = inf_values
-  image = new_image
+  image = lookup[convolve(image, bin2dec)]
 
 result = int(np.sum(image))
 print("Result: {}".format(result))
