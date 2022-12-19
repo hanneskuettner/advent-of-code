2dd60ceb19b9c8cd8c0730a791afcd60957a0bd5
Hannes Küttner <hannes.kuettner@gmx.de>
2022-12-19 18:22:54 +0100

    2022: Add day 12

---
+++ 2022/12/part1.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import numpy as np
+
+from itertools import product
+
+input = open('input.txt', 'r').read().strip()
+
+grid = np.array([[ord(c) for c in l] for l in input.splitlines()])
+start = tuple(np.argwhere(grid == ord('S'))[0])
+goal = tuple(np.argwhere(grid == ord('E'))[0])
+
+grid[start] = ord('a')
+grid[goal] = ord('z')
+
+directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+graph = nx.DiGraph()
+
+for i, j in product(range(grid.shape[0]), range(grid.shape[1])):
+    for di, dj in directions:
+        ni, nj = i + di, j + dj
+        if not (0 <= ni < grid.shape[0] and 0 <= nj < grid.shape[1]):
+            continue
+        if grid[ni, nj] - grid[i, j] <= 1:
+            graph.add_edge((i, j), (ni, nj))
+
+result = nx.shortest_path_length(graph, start, goal)
+print(f"Result: {result}")
